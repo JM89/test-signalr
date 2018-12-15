4e061ba85168296fd2c272a778e9d81819921b17
JM89 <jessica.mottner@gmail.com>
2018-12-15 10:25:01 +0000

    Add wwwroot folder

---
+++ SignalR-Testing/wwwroot/js/chat.js
@@ -0,0 +1,40 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+connection.on("ReceiveMessage", function (msg) {
+    console.log("I received a message! :" + msg);
+});
+
+connection.on("ReceivedMessageFromServer", function (msg) {
+    console.log(msg);
+});
+
+connection.start().catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendNotifications").addEventListener("click", function (event) {
+    connection.invoke("SendMessages", "Helloworld!").catch(function (err) {
+        return console.error(err.toString());
+    });
+});
+
+document.getElementById("triggerAction").addEventListener("click", function (event) {
+    connection.invoke("TriggerAction", "exec").catch(function (err) {
+        return console.error(err.toString());
+    });
+});
+
+var serverToClientHub = new signalR.HubConnectionBuilder().withUrl("/serverToClientHub").build();
+
+serverToClientHub.start();
+serverToClientHub.on("ReceivedMessageFromServer", function (data) {
+    console.log(data);
+});
+
+document.getElementById("registerToGroup1").addEventListener("click", function (event) {
+    serverToClientHub.invoke("JoinGroup", "Grp1").catch(function (err) {
+        return console.error(err.toString());
+    });
+});
